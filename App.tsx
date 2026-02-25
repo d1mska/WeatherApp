3a761b8b84cd828eb47fa1b48cac6b9153a66744
Timo Moilanen <t3moti00@students.oamk.fi>
2026-02-25 18:57:32 +0200

    icon

--- App.tsx
+++ App.tsx
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Image, StatusBar, StyleSheet, Text, View } from 'react-native';
+import { Icon, Searchbar } from 'react-native-paper'
 
 export default function App() {
+  
+  const [searchQuery, setSearch] = useState('');
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{
+      flex: 1,
+      backgroundColor: 'lightblue'
+    }}>
+      <StatusBar backgroundColor='black' />
+      <View style={styles.topBar}>
+        
+        <Text style={styles.title}> Weather</Text>
+        <Searchbar
+      placeholder="Enter city"
+      onChangeText={setSearch}
+      value={searchQuery}/>
+      <View style={{alignItems:"center"}}>
+
+      <Image style={styles.image}
+        source={require("./assets/weather_icon.png")}/>
+        <Text style={{fontSize:40}}>40 C</Text>
+        <Text style={{fontSize:20}}> City</Text>
+      </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  title: {
+    fontSize: 30,
+    fontWeight:"500"
+
   },
+  topBar:{
+    flexDirection: "column",
+    justifyContent:"center",
+    padding: 30
+  },
+  image:{
+    width:200, 
+    height:200
+  }
 });
